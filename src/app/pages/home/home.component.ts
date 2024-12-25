a3aa115682081aa05e77fda0c2e2913aef72a342
ibtissam el hani <ibtissamelhani36@gmail.com>
2024-12-25 11:00:13 +0100

    update landing page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,7 +8,6 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [RouterLink],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
 })
 export class HomeComponent {
 
