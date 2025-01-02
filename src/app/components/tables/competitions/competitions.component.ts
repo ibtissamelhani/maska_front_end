cf03743eb425beabf894bdc944325b924733e1d9
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-01-02 11:29:04 +0100

    adding competitions component

---
+++ src/app/components/tables/competitions/competitions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competitions',
+  standalone: true,
+  imports: [],
+  templateUrl: './competitions.component.html',
+  styles: ``
+})
+export class CompetitionsComponent {
+
+}
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {IndexNavbarComponent} from "../../components/navbars/index-navbar/index-navbar.component";
+import {AuthNavbarComponent} from "../../components/navbars/auth-navbar/auth-navbar.component";
+import {CompetitionsComponent} from "../../components/tables/competitions/competitions.component";
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [
-    IndexNavbarComponent
+    IndexNavbarComponent,
+    CompetitionsComponent
   ],
   templateUrl: './landing.component.html',
   styles: ``
