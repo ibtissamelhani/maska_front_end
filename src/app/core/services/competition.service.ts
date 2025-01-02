0fd55a5122f4a791f33b5652ed540fa9ea4486e2
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-01-02 11:45:17 +0100

    adding competition service & interface

---
+++ src/app/core/services/competition.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {API_BASE_URL} from "../constants/constants";
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Page} from "../interfaces/page";
+import {Competition} from "../interfaces/competition";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompetitionService {
+
+  private apiUrl = API_BASE_URL;
+  constructor(private http: HttpClient) { }
+
+  getPaginatedCompetitions(page: number, size: number): Observable<Page<Competition>> {
+    return this.http.get<Page<Competition>>(`${this.apiUrl}/competition?page=${page}&size=${size}`)
+  }
+}
