4bce97cce5f826d1674e995ba7cd2a2940a04ad5
ibtissam el hani <ibtissamelhani36@gmail.com>
2025-01-06 22:03:17 +0100

    get the competition to edit

--- src/app/admin/Competition/edit-competition/edit-competition.component.ts
+++ src/app/admin/Competition/edit-competition/edit-competition.component.ts
@@ -3,40 +3,59 @@ import {ActivatedRoute, RouterLink} from "@angular/router";
 import {Competition} from "../../../core/interfaces/competition";
 import {CompetitionService} from "../../../core/services/competition.service";
 import {ParticipationService} from "../../../core/services/participation.service";
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-edit-competitions',
   standalone: true,
   imports: [
-    RouterLink
+    ReactiveFormsModule,
+    RouterLink,
+    CommonModule
   ],
   templateUrl: './edit-competition.component.html',
 })
 export class EditCompetitionComponent implements OnInit{
+  competitionForm: FormGroup;
+  isLoading = false;
 
-  competitionId: string = '';
-  competition: Competition | null = null;
-
-  constructor(private route: ActivatedRoute, private competitionService: CompetitionService, private participationService: ParticipationService) {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private competitionService: CompetitionService
+  ) {
+    this.competitionForm = this.fb.group({
+      location: ['', Validators.required],
+      date: ['', Validators.required],
+      minParticipants: [0, [Validators.required, Validators.min(0)]],
+      maxParticipants: [0, [Validators.required, Validators.min(0)]],
+      speciesType: ['', Validators.required],
+      isOpen: [true, Validators.required]
+    });
   }
 
   ngOnInit() {
-    this.competitionId = this.route.snapshot.paramMap.get('id') || '';
-    console.log('Competition ID:', this.competitionId); // Debug log
-    if (this.competitionId) {
-      this.loadCompetitionDetails();
-    }
+    const id = this.route.snapshot.params['id'];
+    this.competitionService.getCompetitionDetails(id).subscribe(competition => {
+      this.competitionForm.patchValue({
+        location: competition.location,
+        date: this.formatDateForInput(competition.date),
+        minParticipants: competition.minParticipants,
+        maxParticipants: competition.maxParticipants,
+        speciesType: competition.speciesType,
+        isOpen: competition.openRegistration
+      });
+    });
   }
 
-  loadCompetitionDetails(): void {
-    this.competitionService.getCompetitionDetails(this.competitionId).subscribe({
-      next: (data) => {
-        this.competition = data;
-      },
-      error: (err) => {
-        console.error('Error fetching competitions details:', err);
-      }
-    });
+  private formatDateForInput(dateString: string): string {
+    const date = new Date(dateString);
+    // Ensure the date is valid
+    if (isNaN(date.getTime())) {
+      return '';
+    }
+    return date.toISOString().split('T')[0];
   }
 
 
